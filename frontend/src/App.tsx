728a3ae9c0fcae92d4bebc5ad9c60b315c3490b1
Pius Prince <117347263+princepius1@users.noreply.github.com>
2022-11-24 11:21:38 +0000

    feat: Add hero component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
