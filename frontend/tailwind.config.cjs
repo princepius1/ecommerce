057129c7bf71e02db9f183704c4220883bbc2018
Pius Prince <117347263+princepius1@users.noreply.github.com>
2022-11-24 11:22:24 +0000

    style: Add extended styling

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-image1": "url('/src/assets/man.jpg')",
+        "hero-image2": "url('/src/assets/man2.jpg')",
+        "hero-image3": "url('/src/assets/man3.jpg')",
+        "hero-image4": "url('/src/assets/man4.jpg')",
+        "hero-image5": "url('/src/assets/man5.jpg')",
+        "hero-image6": "url('/src/assets/man6.jpg')",
+        "hero-image7": "url('/src/assets/man7.jpg')",
+        "hero-image8": "url('/src/assets/sneaker2.jpg')",
+      },
+      height: {
+        128: "35rem",
+      },
+    },
   },
   plugins: [],
 };
