9b550bbc42e7fbe6359f2b64f454c59362d39b8f
Pius Prince <117347263+princepius1@users.noreply.github.com>
2022-11-25 13:57:45 +0000

    style: Add media query screens to tailwind config

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       backgroundImage: {
         "hero-image1": "url('/src/assets/man.jpg')",
